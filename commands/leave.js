3f85032a49999db44c9c1c4624d0ac423d71e22a
Sipos Aron David <sipisipos@gmail.com>
2021-01-12 00:53:13 +0100

    Play and leave command

---
+++ commands/leave.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "leave",
+    description: "The bot leaves the voice channel",
+    async execute(msg, args) {
+        const voiceChannel = msg.member.voice.channel;
+        if (!voiceChannel)
+            return msg.channel.send(
+                "You need to be in a channel to execute this command!"
+            );
+        await voiceChannel.leave();
+        await msg.reply("Bye Bye :smiling_face_with_tear:");
+    },
+};
