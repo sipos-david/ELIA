ae9ff09c943d31109ff21066eb039387eec54c3d
Sipos Aron David <sipisipos@gmail.com>
2021-01-12 21:32:01 +0100

    not used file deleted

--- source/tools/defaultActivity.js
+++
@@ -1,7 +0,0 @@
-const { prefix } = require("../../config.json");
-
-module.exports = function setDefaultActivity(bot) {
-    bot.user.setActivity("your commands! Use " + prefix + "help", {
-        type: "LISTENING",
-    });
-};
