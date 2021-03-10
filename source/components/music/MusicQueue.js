00b723f1feaad67d7429e704033adac8ef2d1317
Sipos Aron David <sipisipos@gmail.com>
2021-03-11 00:35:17 +0100

    song title cache implementation

--- source/components/music/MusicQueue.js
+++ source/components/music/MusicQueue.js
@@ -1,48 +1,47 @@
 const playFromURL = require("./UrlPlay.js");
-const getYoutubeTitle = require("get-youtube-title");
-const getYouTubeID = require("get-youtube-id");
 const ytpl = require("ytpl");
+const ytdl = require("ytdl-core");
 
+/**
+ * A music queue that play's music.
+ */
 class MusicQueue {
     constructor(newELia) {
         this.elia = newELia;
     }
-
+    // --- Music ---
+    /**
+     * Array of Youtube links
+     */
     musicQueueArray = new Array();
-    playingMusic = false;
+    /**
+     * The cache for youtube titles
+     *
+     * Key is youtube ID
+     * Value is the title
+     */
+    titleMap = new Map();
+    /**
+     * Youtube link to the last song
+     */
+    lastSong = null;
+    /**
+     * Youtube link to the current song
+     */
+    currentSong = null;
+    currentSongName = null;
+
+    // --- Discord data ---
     voiceChannel = null;
     connection = null;
-    currentSong = null;
+
+    // --- Flags ---
     paused = false;
-    lastSong = null;
+    playingMusic = false;
     loopSong = false;
     loopQueue = false;
 
-    continuePlayingMusic() {
-        if (this.musicQueueArray.length > 0) {
-            this.playMusicFromQueue();
-        } else {
-            this.stopMusic();
-        }
-    }
-
-    async playYoutubePlaylist(msg, voiceChannel, id) {
-        let playlist = await ytpl(id, {});
-        if (playlist.items.length > 1) {
-            for (let i = 1; i < playlist.items.length; i++) {
-                this.musicQueueArray.push(playlist.items[i].url);
-            }
-        }
-        this.elia.messageComponent.reply(
-            msg,
-            "You stared playing a Youtube Playlist!"
-        );
-
-        this.elia.loggingComponent.log(
-            msg.author.username + " imported a youtube playlist"
-        );
-        this.playMusic(msg, voiceChannel, playlist.items[0].url);
-    }
+    // --- Music control functions ---
 
     async playMusic(msg, voiceChannel, url, title = null) {
         if (voiceChannel == null) return;
@@ -51,6 +50,7 @@ class MusicQueue {
             this.musicQueueArray.unshift(this.currentSong);
 
         this.musicQueueArray.unshift(url);
+        this.cacheYoutubeTitle(url);
         this.playingMusic = true;
 
         if (
@@ -62,17 +62,6 @@ class MusicQueue {
         }
 
         this.playMusicFromQueue(msg, title);
-    }
-
-    async queueMusic(msg, url) {
-        await this.elia.messageComponent.reply(
-            msg,
-            ":musical_note: Queued: ***" + url + "***"
-        );
-        this.elia.loggingComponent.log(msg.author.username + " queued: " + url);
-        if (this.musicQueueArray.push(url) == 1 && this.playingMusic == false) {
-            this.playMusic(msg, msg.member.voice.channel, url);
-        }
     }
 
     async replayMusic(msg) {
@@ -96,6 +85,7 @@ class MusicQueue {
 
     stopMusic(message) {
         this.musicQueueArray = new Array();
+        this.titleMap = new Map();
         this.playingMusic = false;
         this.pauseMusic = false;
         if (this.voiceChannel != null) {
@@ -114,10 +104,166 @@ class MusicQueue {
         this.elia.activityDisplayComponent.setDefault();
     }
 
+    async pauseMusic(msg) {
+        if (
+            !this.paused &&
+            this.voiceChannel != null &&
+            this.connection != null
+        ) {
+            this.paused = true;
+            if (this.connection.dispatcher != null) {
+                this.connection.dispatcher.pause();
+                this.elia.messageComponent.reply(msg, "You paused the music.");
+                this.elia.loggingComponent.log(
+                    msg.author.username + " paused the music"
+                );
+            }
+        } else {
+            this.elia.messageComponent.reply(
+                msg,
+                "You can't pause the music right now."
+            );
+        }
+        msg.delete();
+    }
+
+    async resumeMusic(msg) {
+        if (
+            this.paused &&
+            this.voiceChannel != null &&
+            this.connection != null
+        ) {
+            this.paused = false;
+            if (this.connection.dispatcher != null) {
+                this.elia.messageComponent.reply(
+                    msg,
+                    "You resumed playing the music."
+                );
+                this.elia.loggingComponent.log(
+                    msg.author.username + " resumed playing the music"
+                );
+            }
+            this.connection.dispatcher.resume();
+        } else {
+            this.elia.messageComponent.reply(
+                msg,
+                "You can't resume the music right now."
+            );
+        }
+        msg.delete();
+    }
+
+    playMusicFromQueue(msg, title = null) {
+        if (this.musicQueueArray.length > 0) {
+            this.lastSong = this.currentSong;
+            this.currentSong = this.musicQueueArray.shift();
+            if (this.lastSong == null) this.lastSong = this.currentSong;
+            if (this.loopSong) this.musicQueueArray.unshift(this.currentSong);
+            if (this.loopQueue && !this.loopSong)
+                this.musicQueueArray.push(this.currentSong);
+            this.elia.activityDisplayComponent.setMusicPlaying();
+            playFromURL(
+                this.elia,
+                msg,
+                this.connection,
+                this.currentSong,
+                title
+            );
+        }
+    }
+
+    async queueMusic(msg, url) {
+        await this.elia.messageComponent.reply(
+            msg,
+            ":musical_note: Queued: ***" + url + "***"
+        );
+        this.elia.loggingComponent.log(msg.author.username + " queued: " + url);
+        if (this.musicQueueArray.push(url) == 1 && this.playingMusic == false) {
+            this.playMusic(msg, msg.member.voice.channel, url);
+        }
+    }
+
+    continuePlayingMusic() {
+        if (this.musicQueueArray.length > 0 && this.hasMembersInVoice()) {
+            this.playMusicFromQueue();
+        } else {
+            this.stopMusic();
+        }
+    }
+
+    // --- Queue manipulation functions ---
+
     async skipSong(msg) {
         this.elia.messageComponent.reply(msg, "You skipped a song!");
         this.elia.loggingComponent.log(msg.author.username + " skipped a song");
         this.continuePlayingMusic();
+    }
+
+    shuffleMusic(msg) {
+        if (this.musicQueueArray.length >= 2) {
+            for (var i = this.musicQueueArray.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = this.musicQueueArray[i];
+                this.musicQueueArray[i] = this.musicQueueArray[j];
+                this.musicQueueArray[j] = temp;
+            }
+            this.elia.messageComponent.reply(msg, "You shuffled the music.");
+            this.elia.loggingComponent.log(
+                msg.author.username + " shuffled the music"
+            );
+        }
+    }
+
+    removeFromQueue(number, msg) {
+        if (number.indexOf("-") === -1) {
+            this.removeSongFromQueue(parseInt(number) - 1, msg);
+        } else {
+            let indexes = number.split("-");
+            if (indexes.length <= 1) return;
+            let indexFrom = parseInt(indexes[0]) - 1;
+            let indexTo = parseInt(indexes[1]) - 1;
+            if (indexFrom == indexTo) this.removeSongFromQueue(indexFrom, msg);
+            else if (indexFrom < indexTo)
+                this.removeSongsRangeFromQueue(indexFrom, indexTo, msg);
+            else this.removeSongsRangeFromQueue(indexTo, indexFrom, msg);
+        }
+    }
+
+    async removeSongFromQueue(index, msg) {
+        if (index < 0 || index > this.musicQueueArray.length) return;
+        let removedSong = this.getYoutubeTitleFromCache(
+            this.musicQueueArray[index]
+        );
+        this.removeYoutubeTitleFromCache(this.musicQueueArray[index]);
+        if (index == 0 && this.musicQueueArray.length == 1)
+            this.musicQueueArray = new Array();
+        else this.musicQueueArray.splice(index, 1);
+        this.elia.loggingComponent.log(msg.author.username + " removed a song");
+        this.elia.messageComponent.reply(msg, "Removed song: " + removedSong);
+    }
+
+    async removeSongsRangeFromQueue(indexFrom, indexTo, msg) {
+        let removedSongs = new Array();
+
+        for (let i = indexFrom; i <= indexTo; i++) {
+            removedSongs.push(this.musicQueueArray[i]);
+        }
+
+        if (indexFrom == 0 && indexTo == this.musicQueueArray.length - 1)
+            this.musicQueueArray = new Array();
+        else this.musicQueueArray.splice(indexFrom, indexTo - indexFrom + 1);
+
+        let replyMsg = "***Removed " + removedSongs.length + " songs:***\n";
+        for (let i = 0; i < removedSongs.length; i++) {
+            let removedSong = this.getYoutubeTitleFromCache(removedSongs[i]);
+            this.removeYoutubeTitleFromCache(this.musicQueueArray[index]);
+            replyMsg += removedSong + "\n";
+        }
+        this.elia.loggingComponent.log(
+            msg.author.username + " removed " + removedSongs.length + " songs"
+        );
+
+        msg.reply(replyMsg);
     }
 
     loopCurrentSong(msg) {
@@ -177,16 +323,59 @@ class MusicQueue {
         }
     }
 
+    // --- Youtube functions ---
+
+    async playYoutubePlaylist(msg, voiceChannel, id) {
+        let playlist = await ytpl(id, {});
+        if (playlist.items.length > 1) {
+            for (let i = 1; i < playlist.items.length; i++) {
+                this.musicQueueArray.push(playlist.items[i].url);
+            }
+        }
+        this.elia.messageComponent.reply(
+            msg,
+            "You stared playing a Youtube Playlist!"
+        );
+
+        this.elia.loggingComponent.log(
+            msg.author.username + " imported a youtube playlist"
+        );
+        this.playMusic(msg, voiceChannel, playlist.items[0].url);
+    }
+
+    async cacheYoutubeTitle(url) {
+        ytdl.getInfo(url).then((info) => {
+            if (!this.titleMap.has(url)) {
+                this.titleMap.set(url, info.videoDetails.title);
+            }
+        });
+    }
+
+    getYoutubeTitleFromCache(url) {
+        let title = this.titleMap.get(url);
+        if (typeof title === "undefined") return "Title not cached yet.";
+        else return title;
+    }
+
+    removeYoutubeTitleFromCache(url) {
+        if (this.titleMap.has(url)) {
+            this.titleMap.delete(url);
+        }
+    }
+
+    // --- Queue state getter functions ---
+
+    //TODO better format
     async getQueuedMusic(msg) {
         if (this.currentSong != null) {
-            let currentId = getYouTubeID(this.currentSong);
-            let currenTitle = await this.getYoutubeTitleFromId(currentId);
+            let currenTitle = this.getYoutubeTitleFromCache(this.currentSong);
             let replyMsg = "\n***The current song: ***\n" + currenTitle;
             if (this.musicQueueArray.length > 0) {
                 replyMsg += "\n\n***The current queue:***\n";
                 for (let i = 0; i < this.musicQueueArray.length; i++) {
-                    let id = getYouTubeID(this.musicQueueArray[i]);
-                    let title = await this.getYoutubeTitleFromId(id);
+                    let title = await this.getYoutubeTitleFromCache(
+                        this.musicQueueArray[i]
+                    );
                     replyMsg += i + 1 + ". " + title + "\n";
                 }
             }
@@ -199,119 +388,9 @@ class MusicQueue {
         }
     }
 
-    removeFromQueue(number, msg) {
-        if (number.indexOf("-") === -1) {
-            this.removeSongFromQueue(parseInt(number) - 1, msg);
-        } else {
-            let indexes = number.split("-");
-            if (indexes.length <= 1) return;
-            let indexFrom = parseInt(indexes[0]) - 1;
-            let indexTo = parseInt(indexes[1]) - 1;
-            if (indexFrom == indexTo) this.removeSongFromQueue(indexFrom, msg);
-            else if (indexFrom < indexTo)
-                this.removeSongsRangeFromQueue(indexFrom, indexTo, msg);
-            else this.removeSongsRangeFromQueue(indexTo, indexFrom, msg);
-        }
-    }
-
-    async removeSongFromQueue(index, msg) {
-        if (index < 0 || index > this.musicQueueArray.length) return;
-        let removedSong = await this.getYoutubeTitleFromId(
-            getYouTubeID(this.musicQueueArray[index])
-        );
-        if (index == 0 && this.musicQueueArray.length == 1)
-            this.musicQueueArray = new Array();
-        else this.musicQueueArray.splice(index, 1);
-        this.elia.loggingComponent.log(msg.author.username + " removed a song");
-        this.elia.messageComponent.reply(msg, "Removed song: " + removedSong);
-    }
-
-    async removeSongsRangeFromQueue(indexFrom, indexTo, msg) {
-        let removedSongs = new Array();
-
-        for (let i = indexFrom; i <= indexTo; i++) {
-            removedSongs.push(this.musicQueueArray[i]);
-        }
-
-        if (indexFrom == 0 && indexTo == this.musicQueueArray.length - 1)
-            this.musicQueueArray = new Array();
-        else this.musicQueueArray.splice(indexFrom, indexTo - indexFrom + 1);
-
-        let replyMsg = "***Removed " + removedSongs.length + " songs:***\n";
-        for (let i = 0; i < removedSongs.length; i++) {
-            let removedSong = await this.getYoutubeTitleFromId(
-                getYouTubeID(removedSongs[i])
-            );
-            replyMsg += removedSong + "\n";
-        }
-        this.elia.loggingComponent.log(
-            msg.author.username + " removed " + removedSongs.length + " songs"
-        );
-
-        msg.reply(replyMsg);
-    }
-
-    getYoutubeTitleFromId(id) {
-        return new Promise((resolve, reject) => {
-            getYoutubeTitle(id, function (err, title) {
-                resolve(title);
-            });
-        });
-    }
-
-    async pauseMusic(msg) {
-        if (
-            !this.paused &&
-            this.voiceChannel != null &&
-            this.connection != null
-        ) {
-            this.paused = true;
-            if (this.connection.dispatcher != null) {
-                this.connection.dispatcher.pause();
-                this.elia.messageComponent.reply(msg, "You paused the music.");
-                this.elia.loggingComponent.log(
-                    msg.author.username + " paused the music"
-                );
-            }
-        } else {
-            this.elia.messageComponent.reply(
-                msg,
-                "You can't pause the music right now."
-            );
-        }
-        msg.delete();
-    }
-
-    async resumeMusic(msg) {
-        if (
-            this.paused &&
-            this.voiceChannel != null &&
-            this.connection != null
-        ) {
-            this.paused = false;
-            if (this.connection.dispatcher != null) {
-                this.elia.messageComponent.reply(
-                    msg,
-                    "You resumed playing the music."
-                );
-                this.elia.loggingComponent.log(
-                    msg.author.username + " resumed playing the music"
-                );
-            }
-            this.connection.dispatcher.resume();
-        } else {
-            this.elia.messageComponent.reply(
-                msg,
-                "You can't resume the music right now."
-            );
-        }
-        msg.delete();
-    }
-
     async getCurrentSong(msg) {
         if (this.currentSong != null) {
-            let currentId = getYouTubeID(this.currentSong);
-            let currenTitle = await this.getYoutubeTitleFromId(currentId);
+            let currenTitle = this.getYoutubeTitleFromCache(this.currentSong);
             msg.reply(
                 "***The current song is:***\n\n" +
                     currenTitle +
@@ -326,37 +405,17 @@ class MusicQueue {
         }
     }
 
-    shuffleMusic(msg) {
-        if (this.musicQueueArray.length >= 2) {
-            for (var i = this.musicQueueArray.length - 1; i > 0; i--) {
-                var j = Math.floor(Math.random() * (i + 1));
-                var temp = this.musicQueueArray[i];
-                this.musicQueueArray[i] = this.musicQueueArray[j];
-                this.musicQueueArray[j] = temp;
-            }
-            this.elia.messageComponent.reply(msg, "You shuffled the music.");
-            this.elia.loggingComponent.log(
-                msg.author.username + " shuffled the music"
-            );
-        }
-    }
+    // --- Discord state checker functions ---
 
-    playMusicFromQueue(msg, title = null) {
-        if (this.musicQueueArray.length > 0) {
-            this.lastSong = this.currentSong;
-            this.currentSong = this.musicQueueArray.shift();
-            if (this.lastSong == null) this.lastSong = this.currentSong;
-            if (this.loopSong) this.musicQueueArray.unshift(this.currentSong);
-            if (this.loopQueue && !this.loopSong)
-                this.musicQueueArray.push(this.currentSong);
-            playFromURL(
-                this.elia,
-                msg,
-                this.connection,
-                this.currentSong,
-                title
-            );
-        }
+    hasMembersInVoice() {
+        if (
+            this.voiceChannel != null &&
+            this.voiceChannel.members.has(this.elia.bot.user.id) &&
+            this.voiceChannel.members.size == 1
+        ) {
+            this.elia.loggingComponent.log("Elia was left alone...");
+            return false;
+        } else return true;
     }
 }
 
--- source/components/music/UrlPlay.js
+++ source/components/music/UrlPlay.js
@@ -1,44 +1,36 @@
 const ytdl = require("ytdl-core");
 
 /**
- * Play's a song from anURL
+ * Play's a song from an URL
  *
  * @param {*} elia an ELIA object
  * @param {*} msg a Discord message
  * @param {*} connection a Discord connection
  * @param {*} url an URL in string
- * @param {*} title string
+ * @param {*} title the title of the song
  */
-module.exports = async function playFromURL(
-    elia,
-    msg,
-    connection,
-    url,
-    title = null
-) {
+module.exports = async function playFromURL(elia, msg, connection, url, title) {
     const stream = ytdl(url, { filter: "audioonly" });
 
-    elia.activityDisplayComponent.setMusicPlaying();
+    connection
+        .play(stream, {
+            seek: 0,
+            volume: parseFloat(elia.dataComponent.getMusicVolume()),
+        })
+        .on("finish", () => {
+            elia.musicComponent.musicQueue.continuePlayingMusic();
+        });
+
+    elia.musicComponent.musicQueue.cacheYoutubeTitle(url);
 
     if (msg != null) {
-        connection
-            .play(stream, {
-                seek: 0,
-                volume: parseFloat(
-                    elia.dataComponent.getMusicVolume(msg.guild.id)
-                ),
-            })
-            .on("finish", () => {
-                elia.musicComponent.musicQueue.continuePlayingMusic();
-            });
-
-        if (title == null) {
-            await elia.messageComponent.reply(
+        if (title == null)
+            elia.messageComponent.reply(
                 msg,
                 ":musical_note: Now Playing ***" + url + "***"
             );
-        } else {
-            await elia.messageComponent.reply(
+        else
+            elia.messageComponent.reply(
                 msg,
                 ":musical_note: Now Playing ***" +
                     title +
@@ -46,33 +38,6 @@ module.exports = async function playFromURL(
                     url +
                     "***"
             );
-        }
-
-        console.log(msg.author.username + " played: " + url);
-    } else {
-        connection
-            .play(stream, {
-                seek: 0,
-                volume: parseFloat(elia.dataComponent.getMusicVolume()),
-            })
-            .on("finish", () => {
-                elia.musicComponent.musicQueue.continuePlayingMusic();
-            });
-
-        if (title == null) {
-            elia.bot.channels.cache
-                .get(elia.dataComponent.getBotSpamChannelId(msg.guild.id))
-                .send(":musical_note: Now Playing ***" + url + "***");
-        } else {
-            elia.bot.channels.cache
-                .get(elia.dataComponent.getBotSpamChannelId(msg.guild.id))
-                .send(
-                    ":musical_note: Now Playing ***" +
-                        title +
-                        "*** at ***" +
-                        url +
-                        "***"
-                );
-        }
+        elia.loggingComponent.log(msg.author.username + " played: " + url);
     }
 };
